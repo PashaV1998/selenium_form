b93faaedad993370ab77e4c2219c174ea9ad8dde
PashaV1998 <fcbfcb.ua@gmail.com>
2020-10-27 14:59:44 +0200

    pashacommit

--- selenium_form.js
+++ selenium_form.js
@@ -4,3 +4,28 @@
 // 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
 // 2. Login with invalid creds and check validation error
 // 3. Logout from app and assert you successfully logged out
+/// <reference types="Cypress" />
+it('Login with valid data', () => {
+    cy.visit('https://the-internet.herokuapp.com/login')
+    cy.get('[id="username"]').type('tomsmith')
+    cy.get('[id="password"]').type('SuperSecretPassword!')
+    cy.get('.radius').click()
+    cy.get('.flash.success').contains('You logged into a secure area!').should('exist')
+})
+
+it('Login with invalid data', () => {
+    cy.visit('https://the-internet.herokuapp.com/login')
+    cy.get('[id="username"]').type('pasha')
+    cy.get('[id="password"]').type('pasha')
+    cy.get('.radius') .click() 
+    cy.get('.flash.error').contains('Your username is invalid!').should('exist')
+})
+
+it('Login with valid data', () => {
+    cy.visit('https://the-internet.herokuapp.com/login')
+    cy.get('[id="username"]').type('tomsmith')
+    cy.get('[id="password"]').type('SuperSecretPassword!')
+    cy.get('.radius').click()
+    cy.get('.button.secondary.radius').click()
+    cy.get('[id="flash"]').contains('You logged out of the secure area!').should('exist')
+})
